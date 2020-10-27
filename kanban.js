73d48df5c30c4c159e7c259451e16cb76590cf6f
srubey <scrubey@pdx.edu>
2020-10-26 19:03:08 -0700

    Baseline for adding stuff to the foreground.

--- kanban.js
+++ kanban.js
@@ -2,8 +2,11 @@ const foreground = document.getElementById("main");
 const addTaskBtn = document.getElementById("addTask");
 
 //add an editable text-box when Add Task button is clicked
-addTaskBtn.addEventListener("click", addTaskBox);
+addTaskBtn.addEventListener("click", function(e){
+    addTaskBox();
+});
 
 function addTaskBox(e){
     addTaskBtn.value="pressed";
+
 }
