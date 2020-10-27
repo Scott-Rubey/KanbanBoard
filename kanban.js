399d1e06a429cf8a26e67430957bf9cfdb25952c
srubey <scrubey@pdx.edu>
2020-10-26 18:59:56 -0700

    Trying to add things to the main page to the right.  Just tinkering at this point.

--- kanban.js
+++ kanban.js
@@ -2,16 +2,8 @@ const foreground = document.getElementById("main");
 const addTaskBtn = document.getElementById("addTask");
 
 //add an editable text-box when Add Task button is clicked
-addTaskBtn.addEventListener("click", function(e){
-    foreground.appendChild(addTaskBox());
-});
+addTaskBtn.addEventListener("click", addTaskBox);
 
 function addTaskBox(e){
-    var newTaskBox = backlog.createElement('textarea');
-    textarea.type = "text";
-    textarea.rows="14";
-
-    foreground.innerHTML += "Hello";
-    
-    return textarea;
+    addTaskBtn.value="pressed";
 }
