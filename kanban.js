36fd84369439558353ae60ca454dcb01c18b3cbc
srubey <scrubey@pdx.edu>
2020-10-26 17:59:31 -0700

    Added active animation to addTask button

--- kanban.js
+++ kanban.js
@@ -1,1 +1,17 @@
-//put code here
+const foreground = document.getElementById("main");
+const addTaskBtn = document.getElementById("addTask");
+
+//add an editable text-box when Add Task button is clicked
+addTaskBtn.addEventListener("click", function(e){
+    foreground.appendChild(addTaskBox());
+});
+
+function addTaskBox(e){
+    var newTaskBox = backlog.createElement('textarea');
+    textarea.type = "text";
+    textarea.rows="14";
+
+    foreground.innerHTML += "Hello";
+    
+    return textarea;
+}
