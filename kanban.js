46afd17a722c756c7cf0b4507c1d259c9e198b43
srubey <scrubey@pdx.edu>
2020-10-28 13:53:36 -0700

    Add task button creates new form.  Need to format, add more fields, set to foreground on page.

--- kanban.js
+++ kanban.js
@@ -3,14 +3,36 @@ const addTaskBtn = document.getElementById("addTask");
 
 //add an editable text-box when Add Task button is clicked
 addTaskBtn.addEventListener("click", function(e){
-    foreground.append(addTaskBox());
+    foreground.append(createForm());
 });
 
-function addTaskBox(e){
-    var newTask = document.createElement('textarea');
-    newTask.id="newTask";
-    newTask.rows=10;
-    newTask.cols=30;
+function createForm(e){
+    var newTaskForm = document.createElement('form');
+    newTaskForm.id="newTaskForm";
 
-    return newTask;
+    //add a header to the new form
+    newTaskForm.innerHTML += "<h2 id='newTaskFormHeader'>Create New Task</h2>";
+
+    //add form elements
+    addTaskLabel(newTaskForm);
+    addTaskBox(newTaskForm);
+
+    return newTaskForm;
+}
+
+function addTaskLabel(newTaskForm){
+    var taskLabel = document.createTextNode("Task:");
+    taskLabel.id="taskLabel";
+    newTaskForm.appendChild(taskLabel);
+
+    return newTaskForm;
+}
+
+function addTaskBox(newTaskForm){
+    var taskBox = document.createElement("input");
+    taskBox.type="text";
+    taskBox.id="newTaskBox";
+    newTaskForm.appendChild(taskBox); 
+
+    return newTaskForm;
 }
