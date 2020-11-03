26cc1e3e00aad1a18c6445bec6f45ef68f22c0bb
srubey <scrubey@pdx.edu>
2020-11-02 18:53:38 -0800

    Completed AddTask form, made it a popup, draggable.  Dead 'cancel' button.

--- kanban.js
+++ kanban.js
@@ -1,14 +1,15 @@
-const foreground = document.getElementById("main");
+const main = document.getElementById("main");
 const addTaskBtn = document.getElementById("addTask");
 
 //add an editable text-box when Add Task button is clicked
 addTaskBtn.addEventListener("click", function(e){
-    foreground.append(createForm());
+    main.appendChild(createForm());
 });
 
 function createForm(e){
     var newTaskForm = document.createElement('form');
-    newTaskForm.id="newTaskForm";
+    newTaskForm.setAttribute("id", "newTaskForm");
+    newTaskForm.setAttribute("class", "popup");
 
     //add a header to the new form
     newTaskForm.innerHTML += "<h2 id='newTaskFormHeader'>Create New Task</h2>";
@@ -17,7 +18,10 @@ function createForm(e){
     addTaskBox(newTaskForm);
     addPriorityBox(newTaskForm);
     addDescriptionBox(newTaskForm);
-    addSubmitButton(newTaskForm);
+    addButtons(newTaskForm);
+
+    //make form draggable
+    drag(newTaskForm);
 
     return newTaskForm;
 }
@@ -27,6 +31,7 @@ function addTaskBox(newTaskForm){
     taskBox.setAttribute("type", "text");
     taskBox.setAttribute("id", "newTaskBox");
     taskBox.setAttribute("placeholder", "Enter task");
+    taskBox.setAttribute("required", true);
     newTaskForm.appendChild(taskBox); 
     newTaskForm.innerHTML += "<br>";
 
@@ -80,16 +85,67 @@ function addDescriptionBox(newTaskForm){
     return newTaskForm;
 }
 
-function addSubmitButton(newTaskForm){
+function addButtons(newTaskForm){
     //create submit button
     var submit = document.createElement("input");
     submit.setAttribute("type", "submit");
     submit.setAttribute("id", "submitBtn");
     submit.setAttribute("value", "Submit");
+    submit.setAttribute("class", "button form");
+
+    //create reset button
+    var reset = document.createElement("input");
+    reset.setAttribute("type", "button");
+    reset.setAttribute("id", "resetBtn");
+    reset.setAttribute("value", "Cancel");
+    reset.setAttribute("class", "button form"); 
 
     //add to form
+    newTaskForm.appendChild(reset);
     newTaskForm.appendChild(submit);
     newTaskForm.innerHTML += "<br>";
 
     return newTaskForm;
+}
+
+//function credit: https://www.w3schools.com/howto/howto_js_draggable.asp
+function drag(form) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("newTaskFormHeader")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById("newTaskFormHeader").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    form.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(form) {
+    form = form || window.event;
+    form.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = form.clientX;
+    pos4 = form.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(form) {
+    form = form || window.event;
+    form.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - form.clientX;
+    pos2 = pos4 - form.clientY;
+    pos3 = form.clientX;
+    pos4 = form.clientY;
+    // set the element's new position:
+    form.style.top = (form.offsetTop - pos2) + "px";
+    form.style.left = (form.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
