54578a76da4c1ea855562a54f6822904c6a254b4
tjnuccio <tj_nuccio@yahoo.com>
2020-11-20 11:18:32 -0800

    nothing important

--- includes/projects.php
+++ includes/projects.php
@@ -1,8 +1,18 @@
 <?php include('config.php');
 
 $projects = pg_query('SELECT projectname FROM project WHERE userid ='.$_SESSION['userid']);
-$res = pg_fetch_all($projects);
+$res = pg_fetch_all($projects);                                                             //Array of user's projects
 
-echo json_encode($res);
+$q = pg_query('SELECT projectid FROM project WHERE userid ='.$_SESSION['userid']);
+$collab = array(); 
+
+$count = count($res);
+
+for($i = 0; $i < $count; $i++) {
+    $add = pg_fetch_result($q, 0);
+    array_push($collab, array($add=> "name"));
+}
+
+echo json_encode($collab);
 
 ?>
