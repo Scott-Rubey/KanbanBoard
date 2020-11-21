43c739103c0e787c2459884854eea7d0ee9b48cc
tjnuccio <tj_nuccio@yahoo.com>
2020-11-21 12:27:40 -0800

    Edit project modal is now setup. New project button on projects page now works.

---
+++ includes/edit-project.php
@@ -0,0 +1,52 @@
+<?php include('./config.php');
+
+$projname = $_POST['newprojectname'];
+$collab = json_decode($_POST['collaborators']);
+$projid = $_POST['projectid'];
+
+$proj = pg_query($conn, "SELECT * FROM project WHERE userid = ".$_SESSION['userid']." AND projectid ="."'".$projid."'");         //Gets current user's projects
+$oldName = pg_fetch_result($proj, 'projectname');                                                                                       //Check for userid and proj to avoid duplicate proj names by different users
+
+if(strcmp($projname, $oldName) == 0) {                                                                   //Project already exists by the supplied name 
+
+    http_response_code(200); 
+    echo json_encode(array("success"=>true, "duplicate"=>true)); 
+
+} else {
+
+    pg_query($conn, "UPDATE project SET projectname = "."'".$projname."'"." WHERE projectid = ". $projid);
+
+    if(!empty($collab)) {                                                                              
+        
+        foreach($collab as $c) {                                                                                    //Loop to add collaborators
+            $newCollaborator = pg_query($conn, "SELECT * FROM person WHERE email = "."'".$c."'");                    //Check if collaborator is in person table
+            $newEmail = pg_fetch_result($newCollaborator, "email");                                                  //Fetch collaborator email 
+            if(strcmp($newEmail, $c) == 0) {                                                                        //Collaborator email exists
+                $curUser = pg_query($conn, "SELECT email FROM person WHERE userid = ".$_SESSION['userid']);         //Check current user's email against collaborator emails provided
+                if(strcmp(pg_fetch_result($curUser, "email"), $c) == 0) {
+                    //User tried to add self as collaborator 
+                    continue; 
+                } else {
+                    $test = pg_query($conn, "SELECT userid FROM person WHERE email = "."'".$c."'");                  //Check if collaborator is in person table
+                    $newUserId = pg_fetch_result($test, "userid");
+                    $duplicateCollab = pg_query($conn, "SELECT * FROM collaborators WHERE projectid = ".$projid." AND userid = ".$newUserId);
+                    if(pg_fetch_result($duplicateCollab, "userid") == "") {                                         //Collaborator not already assigned to project, add
+                        $params = array(                                    
+                            "projectid"=>$projid,
+                            "userid"=>$newUserId
+                        );  
+                        $insert = pg_insert($conn, 'collaborators', $params);                                       //Insert collaborator into database 
+                    }
+                }
+            } else {
+                //Email not found in person table 
+                
+            }
+        }
+    }
+    
+    http_response_code(200); 
+    echo json_encode(array("success"=>true, "duplicate"=>false, 'projectid'=>$projid)); 
+
+}
+?>
--- scripts/projects.js
+++ scripts/projects.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             console.log(1)
 
             if(result.length > 0) {
-                //document.getElementById('projects-CTA').style.display = 'none'
+                document.getElementById('projects-CTA').style.display = 'none'
             }
 
             for(var i = 0; i < result.length; i++) {
@@ -36,6 +36,7 @@ $(document).ready(function() {
                     td1.innerHTML = result[i].collaborators                     //Add collaborators 
                 else 
                     td1.innerHTML = "None"
+
                 var td2 = document.createElement('td')
                 var date = result[i].modified.split("-")                        //Split to reformat date from psql                  
                 td2.innerHTML = date[1] + '/' + date[2] + '/' + date[0]         //Add data modified 
@@ -46,6 +47,7 @@ $(document).ready(function() {
                 btn.classList.add('btn-light')
                 btn.setAttribute('data-toggle', 'modal')
                 btn.setAttribute('data-target', '#exampleModal')
+                btn.setAttribute('id', result[i].projectid)
 
                 td3.appendChild(btn)
 
@@ -54,11 +56,55 @@ $(document).ready(function() {
                 tr.appendChild(td2)
                 tr.appendChild(td3)
                 tbody.appendChild(tr)
+                
             }
+
+            $('#list-items').on('click', 'button', function(e) {
+
+                var projectId = this.id
+                
+                $('#saveChanges').on('click', function() {
+                    var newProjName = $('#newProjectName').val()
+                    var newCollab = $('#newCollabs').val()
+
+                    if(newCollab) {
+                        newCollab = newCollab.split(";")
+                        for(var i = 0; i < newCollab.length; i++)
+                            newCollab[i] = newCollab[i].trim() 
+                    }
+                
+                    var formData= {
+                        'newprojectname': newProjName,
+                        'collaborators': JSON.stringify(newCollab),
+                        'projectid': projectId
+                    }
+
+                    $.ajax({
+                        type: 'POST', 
+                        url: '../includes/edit-project.php', 
+                        data: formData,
+                    })
+                    .done(function(data) {
+                        //console.log(data)
+                        var data = JSON.parse(data)
+                        if(data.success) {
+                            if(data.duplicate == true) {
+                                alert("This project already has that name! Try using a new name.")
+                            } else {
+                                location.reload()
+                            }
+                        }
+                    })
+                    .fail(function(data) {
+                        console.log(data)
+                    })
+                    
+                })
+
+            })
         } else {
             document.getElementById('list-header').style.display = 'none'
             document.getElementById('list-items').style.display = 'none'
-            
         }
 
     })
@@ -66,3 +112,4 @@ $(document).ready(function() {
         console.log('Projects could not be retrieved')
     })
 })
+
